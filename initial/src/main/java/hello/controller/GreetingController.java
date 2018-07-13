455af1c204b3e13d814bf185356f836414c39de3
Vesa Tanhua-Tyrkkö <vesa.tanhua-tyrkko@opr-vakuus.fi>
2018-07-13 12:07:42 +0300

    logging and swagger added

--- initial/src/main/java/hello/controller/GreetingController.java
+++ initial/src/main/java/hello/controller/GreetingController.java
@@ -1,18 +1,27 @@
-package hello;
+package hello.controller;
 
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import hello.businessobject.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 @RestController
+@Slf4j
 public class GreetingController {
+    //Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name){
+        log.info("Hello World");
         return new Greeting(counter.incrementAndGet(), String.format(template,name));
     }
 }
